f1ebaafff82ca0801919884060aa63170cc85c2f
Shashank Malhotra <malhotrazmr@gmai.com>
2023-06-02 12:07:19 +0530

    tooltip

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,15 +17,17 @@ import Button from "@mui/material/Button";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Tooltip from "@mui/material/Tooltip";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
 import {
   AccountCircle,
-  AddShoppingCart,
   Home,
-  HomeMax,
-  HomeMaxOutlined,
-  HomeMaxSharp,
-  HomeMiniTwoTone,
-  HomeRounded,
   LocalMall,
   LocalOffer,
 } from "@mui/icons-material";
@@ -83,19 +85,6 @@ const navItems = [
     ),
     path: "/api",
   },
-  // {
-  //   label: (
-  //     <Button
-  //       variant="text"
-  //       startIcon={
-  //         <Badge badgeContent={0} color="primary">
-  //           <ShoppingCartIcon />
-  //         </Badge>
-  //       }
-  //     ></Button>
-  //   ),
-  //   path: "/shoppingcart",
-  // },
 ];
 
 const theme = createTheme();
@@ -114,6 +103,16 @@ function Navbar(props) {
   const { window } = props;
   const items = useSelector((state) => state.cart);
   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  // Account Menu
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
@@ -160,22 +159,7 @@ function Navbar(props) {
               textAlign: "center",
             }}
             to={"/shoppingcart"}
-          >
-            {/* <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText
-                primary={
-                  <Button
-                    variant="text"
-                    startIcon={
-                      <Badge badgeContent={items.length} color="primary">
-                        <ShoppingCartIcon />
-                      </Badge>
-                    }
-                  ></Button>
-                }
-              />
-            </ListItemButton> */}
-          </Link>
+          ></Link>
         </ListItem>
       </List>
     </Box>
@@ -188,11 +172,10 @@ function Navbar(props) {
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
-        position="static"
+        position="fixed"
         sx={{
           bgcolor: "black",
-          position: "fixed",
-          zIndex: 9999,
+          zIndex: 1100,
         }}
         component="nav"
       >
@@ -274,9 +257,92 @@ function Navbar(props) {
                 }
               </Link>
             </Button>
+            <Button>
+              <Tooltip title="Account settings">
+                <IconButton
+                  onClick={handleClick}
+                  size="small"
+                  aria-controls={open ? "account-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                >
+                  <Avatar sx={{ width: 32, height: 32 }}></Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 2.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&:before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                // transformOrigin={{ horizontal: "right", vertical: "top" }}
+                // anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> Profile
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <PersonAdd fontSize="small" />
+                  </ListItemIcon>
+                  Add another account
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Settings fontSize="small" />
+                  </ListItemIcon>
+                  Settings
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Logout
+                </MenuItem>
+              </Menu>
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
+
       <Box component="nav">
         <Drawer
           container={container}
